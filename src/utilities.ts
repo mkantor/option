55e6844dd4d501c2198a49105b5ecd9ff72722cf
Matt Kantor <the.matt.kantor@gmail.com>
2026-05-12 08:00:31 -0400

    Add `sequence` combinator

--- src/utilities.ts
+++ src/utilities.ts
@@ -42,3 +42,57 @@ export const map = <Value, NewValue>(
     none: _ => none,
     some: value => makeSome(f(value)),
   })
+
+/**
+ * Transform an array of `Option`s into a `Some` containing an array, or a
+ * `None` if any of the `options` are `None`.
+ */
+export const sequence = <const Options extends readonly Option<unknown>[]>(
+  options: Options,
+): SequenceOutput<Options> => {
+  const firstOption = options[0]
+
+  const returnValue =
+    firstOption === undefined
+      ? makeSome([])
+      : options.reduce<
+          // Unfortunately TypeScript doesn't keep track of the specific value
+          // types in the `Options` type parameter—instead it falls back to the
+          // concrete constraint type.
+          Option<readonly unknown[]>
+        >(
+          (combinedOption, currentOption) =>
+            flatMap(combinedOption, combinedValue =>
+              map(currentOption, currentValue => [
+                ...combinedValue,
+                currentValue,
+              ]),
+            ),
+          map(firstOption, value => [value]),
+        )
+
+  // The above `reduce` callback is guaranteed to produce an `Option` whose
+  // value is an array of the same length as `Options`, but that's not provable
+  // in TypeScript's type system.
+  return returnValue as SequenceOutput<Options>
+}
+type SequenceOutput<Options extends readonly Option<unknown>[]> = Option<
+  ReduceNevers<{
+    -readonly [Index in keyof Options]: ValueOf<Options[Index]>
+  }>
+> &
+  unknown // Hide `SequenceOutput` from type info.
+
+type ValueOf<SpecificOption extends Option<unknown>> =
+  SpecificOption extends Some<infer Value> ? Value : never
+
+/**
+ * Convert uninhabited tuples to `never` (e.g. `[never]` becomes `never`).
+ */
+type ReduceNevers<Tuple extends readonly unknown[]> = Tuple extends Tuple // Distribute over unions.
+  ? {
+      [Index in keyof Tuple]: Tuple[Index] extends never ? unknown : never
+    }[number] extends never
+    ? Tuple
+    : never
+  : never
